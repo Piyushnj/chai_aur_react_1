0bcc57b7163e87b4172f4df8b8f9d04e15034770
Piyushnj <p.1ghatole@gmail.com>
2024-10-17 13:04:44 -0400

    first ci

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello Pooja adada</h1>
+     <h1>Hello Poojaadada adada</h1>
+    </div>
+  );
+}
+
+export default App;
